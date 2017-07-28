dd557bb634244f784cc9043fc0f9747d69594cee
Dan <DanielWRoberts@outlook.com>
2017-07-27 19:07:16 -0700

    Registered HBS helpers and partials
    Finished video section 5: 44 "Advanced Templating"

--- server.js
+++ server.js
@@ -5,23 +5,30 @@ const hbs = require('hbs');
 
 let app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", (text) => {
+  return text.toUpperCase();
+});
 
 const PORT = 3000; // process.argv[2] || 3000
 
 app.get("/", (req, res, next) => {
   res.render("home.hbs", {
     pageTitle: "Home Page",
-    currentYear: new Date().getFullYear(),
     welcomeMessage: "Hello"
   });
 });
 
 app.get('/about', (req, res, next) => {
   res.render("about.hbs", {
-    pageTitle: "About Page",
-    currentYear: new Date().getFullYear()
+    pageTitle: "About Page"
   });
 });
 
