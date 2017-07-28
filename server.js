8a12ca20e7f1d005deff7b469c461e624dd02494
Dan <DanielWRoberts@outlook.com>
2017-07-27 19:40:21 -0700

    Added Express middleware
    Finished video section 5: 45 "Express Middleware"

--- server.js
+++ server.js
@@ -2,11 +2,34 @@
 
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
+
+// logger
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", (err) => {
+    if (err) {
+      console.log("Unable to append to 'server.log'.");
+    }
+  });
+
+  next();
+});
+
+// maintenance mode
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs");
+//   // no call to next
+// });
+
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerHelper("getCurrentYear", () => {
